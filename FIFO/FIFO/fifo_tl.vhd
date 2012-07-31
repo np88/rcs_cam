a4f2e250b256f6be844ad731ee9cae75426f2464
np88 <nadja.peters@tum.de>
2012-07-31 17:51:04 +0200

    toplevel for fifo added, no entity definition

--- FIFO/FIFO/fifo_tl.vhd
+++ FIFO/FIFO/fifo_tl.vhd
@@ -29,14 +29,51 @@ use IEEE.STD_LOGIC_1164.ALL;
 --library UNISIM;
 --use UNISIM.VComponents.all;
 
-entity fifo_tl is
-
+entity fifo_tl is PORT (
+    clk : IN STD_LOGIC;
+    rst : IN STD_LOGIC;
+    din : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
+    wr_en : IN STD_LOGIC;
+    rd_en : IN STD_LOGIC;
+    dout : OUT STD_LOGIC_VECTOR(7 DOWNTO 0);
+    full : OUT STD_LOGIC;
+    empty : OUT STD_LOGIC;
+    underflow : OUT STD_LOGIC;
+    data_count : OUT STD_LOGIC_VECTOR(15 DOWNTO 0)
+  );
 end fifo_tl;
 
 architecture Behavioral of fifo_tl is
+-- fifo component
+COMPONENT fifo_one_clock_domain
+  PORT (
+    clk : IN STD_LOGIC;
+    rst : IN STD_LOGIC;
+    din : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
+    wr_en : IN STD_LOGIC;
+    rd_en : IN STD_LOGIC;
+    dout : OUT STD_LOGIC_VECTOR(7 DOWNTO 0);
+    full : OUT STD_LOGIC;
+    empty : OUT STD_LOGIC;
+    underflow : OUT STD_LOGIC;
+    data_count : OUT STD_LOGIC_VECTOR(15 DOWNTO 0)
+  );
+END COMPONENT;
+
 
 begin
-
+fifo: fifo_one_clock_domain PORT MAP (
+    clk => clk,
+    rst => rst,
+    din => din,
+    wr_en => wr_en,
+    rd_en => rd_en,
+    dout => dout,
+    full => full,
+    empty => empty,
+    underflow => underflow,
+    data_count => data_count
+  );
 
 end Behavioral;
 
